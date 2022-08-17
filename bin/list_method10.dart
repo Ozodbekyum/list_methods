8a09d6f6d38fcfce3a4bf382a59d9c483c9b61d2
Ozodbekyum <hello7ozod@gamil.com>
2022-08-17 10:03:27 +0500

    meth10

--- bin/list_method10.dart
+++ bin/list_method10.dart
@@ -1,10 +1,32 @@
 /*
   create function with arguments
-      A list of zeros and ones is given. Find how many ones and how many zeros there are and return to the list form.
+      A list of zeros and ones is given. 
+      Find how many ones and how many zeros there are and return to the list form.
     Args:
         list1(list): parameter
     Returns:
         list: return answer
 */
+List func(List list1) {
+  int x = 0;
+  int w = 0;
+  int i = 0;
+  List q = [];
+  int b = list1.length;
+  while (b > x) {
+    if (list1[x] == 0) {
+      w++;
+    }
+    if (list1[x] == 1) {
+      i++;
+    }
+    x++;
+  }
+  q.add(i);
+  q.add(w);
+  return q;
+}
 
-void main() {}
+void main() {
+  print(func([1, 0, 0, 0, 1, 0, 1, 0]));
+}
