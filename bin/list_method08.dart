238733f471d5332c167c241e6cb7266c64aceec8
Ozodbekyum <hello7ozod@gamil.com>
2022-08-17 09:23:31 +0500

    meth08

--- bin/list_method08.dart
+++ bin/list_method08.dart
@@ -1,10 +1,19 @@
 /*
   create function with arguments
-    A list called "fruits" is given and contains at least one "apple". Remove the apples from the list and return the list.
+    A list called "fruits" is given and contains at least one "apple". 
+    Remove the apples from the list and return the list.
     Args:
         fruits(list): parameter
     Returns:
         list: return answer
 */
+List func(List frut) {
+  int c = frut.length;
+  while (frut.remove('apple')) {}
 
-void main() {}
+  return frut;
+}
+
+void main() {
+  print(func(['apple', 'apple', 'pear', 'apple', 'pear']));
+}
